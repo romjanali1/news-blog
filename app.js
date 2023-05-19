fac10c313fcd85f9faf8f6f9339740a038ea7b29
romjanali1 <mdromgana@gmail.com>
2023-05-19 21:14:23 +0600

    news-navebar adding

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('adddd')
+const newsCategories = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(data => categories(data.data.news_category))
+};
+
+const categories =  datas => {
+    const newsNavbar = document.getElementById('navbarNav')
+    datas.forEach(data => {
+        console.log(data)
+        const navLi = document.createElement('li');
+        navLi.innerHTML = `
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">${data.category_name}</a>
+        </li>
+        
+        `
+        newsNavbar.appendChild(navLi);
+        
+    });
+}
+
+
+
+
+newsCategories();
